861e20dc00206d3f25f40aa39e095cc650f7de9a
Dominik <DominikBorzyszkowski12@gmail.com>
2026-03-11 23:25:04 +0100

    feat: zadanie 4 i 5

--- lab-01/zadanie4/src/Main.java
+++ lab-01/zadanie4/src/Main.java
@@ -5,9 +5,18 @@ class Main
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-        String first = scan.next();
-        String second = scan.next();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
-        System.out.println("\"%" + second + " " + first + "%\"" );
+        if ((a>0) && (b>0) && (c>0)) {
+            if ((a + b > c) && (a + c > b) && (b + c > a)) {
+                System.out.println("TAK");
+            } else {
+                System.out.println("NIE");
+            }
+        } else {
+            System.out.println("BŁĄD");
+        }
     }
 }
