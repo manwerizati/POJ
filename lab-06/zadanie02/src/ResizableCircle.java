c6fd7f479fc47dcc6660388fa2c3ffbe0be4bd45
Dominik <DominikBorzyszkowski12@gmail.com>
2026-06-03 00:14:35 +0200

    feat: zadanie 02

---
+++ lab-06/zadanie02/src/ResizableCircle.java
@@ -0,0 +1,10 @@
+public class ResizableCircle extends Circle implements Resizable {
+    ResizableCircle(double radius) {
+        super(radius);
+    }
+
+    @Override
+    public void resize(int percent) {
+        radius = radius * (percent / 100.0);
+    }
+}
